72082298a8c2781cc5bc0c11cb5090f1215a0ab3
Yelyzaveta <115930443+macquier@users.noreply.github.com>
2023-03-31 20:35:00 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const slider = tns({
+  container: ".header__slider",
+  items: 1,
+  slideBy: "page",
+  autoplay: true,
+  controls: false,
+  autoplayButtonOutput: false,
+  // autoplayText: ["", ""],
+});
